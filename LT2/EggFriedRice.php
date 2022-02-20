7322883b040e4119d343a88d251ea5594ec3254f
Lowelyn Ann B. De Paula <99851965+kcdepaula@users.noreply.github.com>
2022-02-21 01:57:43 +0800

    Update EggFriedRice.php

--- LT2/EggFriedRice.php
+++ LT2/EggFriedRice.php
@@ -1,33 +1,28 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title> Chili con Carne </title>
+    <title> Egg Fried Rice </title>
   </head>
   <body>
-	<h1> Chili con Carne </h1>
-	<img src="ChiliConCarne.jpg" width= 450 height= 300 alt="Chili Con Carne">
+	<h1> Egg Fried Rice </h1>
+	<img src="EggFriedRice.jpg" width= 450 height= 300 alt="Egg Fried Rice">
 	<h2> Ingredients </h2>
 <ul>
-	<li> <p>Ground/minced beef 500g</p> </li>
-	<li> <p>1 Large onion chopped</p> </li>
-	<li> <p>2-3 Cloves of Garlic</p> </li>
-	<li> <p>1-2 Tins of chopped tomatoes 400g</p> </li>
-	<li> <p>Squeeze of tomato puree</p> </li>
-    <li> <p>1 teaspoon of chili powder (or to taste)</p> </li>
-    <li> <p>1 teaspoon of ground cumin</p> </li>
-    <li> <p>dash of Worcester sauce</p> </li>
-    <li> <p>Sprinkle of salt and pepper</p> </li>
-    <li> <p>1 Chopped red pepper</p> </li>
-    <li> <p>1 tin of drained kidney beans 400g</p> </li>
+	<li> <p>Rice(as much as you fancy really!)</p> </li>
+	<li> <p>Half an onion (Or more, again if you fancy it)</p> </li>
+	<li> <p>A tin of sweetcorn</p> </li>
+	<li> <p>A few mushrooms</p> </li>
+	<li> <p>1-2 eggs</p> </li>
 </ul>
 <ol>
 	<h2> Method </h2>
-	<li> <p>1) Fry the onion in a hot pan with oil until nearly brown the add chopped garlic.</p> </li>
-	<li> <p>2) Add the mince and stir until brown - drain any excess fat if desired. </p> </li>
-	<li> <p>3) Add all dried spices and seasoning then reduce heat and add chopped tomatoes. </p> </li>
-	<li> <p>4) Stir well and add tomato puree and worcester sauce then leave to simmer for about an hour (less if you're in a rush) </p> </li>
-	<li> <p>5) Add the chopped red pepper and continue to simmer for 5 mins, then add the tin of drained kidney beans and cook for a further 5 mins. If the chili become to dry at any point just add a bit of water. </p> </li>
-	<li> <p>Serve with rice, jacket potatoes or pasta! </p> </li>
+	<li> <p>Start cooking the rice however you do, and put some oil in a frying pan.</p> </li>
+	<li> <p>Chop the onion into bits, any size really, doesn't matter particularly and throw into the frying pan.</p> </li>
+	<li> <p>Chop the mushrooms and add to the onions.</p> </li>
+	<li> <p>Once the rice is cooked, drain it off and add it to the frying pan. </p> </li>
+	<li> <p>Tip the drained sweetcorn into the pan as well and stir it all up.</p> </li>
+	<li> <p>Break the eggs into the pan (OR beat them together in another bowl and tip into the frying pan) and stir the mixture up. </p> </li>
+	<li> <p>At this point, you need to keep stirring the mixture, keeping the eggs moving. Keep going until the eggs have cooked, should take about 5-­‐8 minutes maybe? </p> </li>
 </ol>
   </body>
 </html>
