d1a4c599e748ac3fc740f02953f137ea9b884013
Lowelyn Ann B. De Paula <99851965+kcdepaula@users.noreply.github.com>
2022-02-21 01:48:43 +0800

    Create SalmonInTheOven.php

---
+++ LT2/SalmonInTheOven.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Salmon in the Oven </title>
+  </head>
+  <body>
+	<h1> Salmon in the Oven </h1>
+	<img src="Salmon.jpg" width= 450 height= 300 alt="Salmon in the Oven">
+    <h2> Ingredients </h2>
+<ul>
+	<li> <p>1 fillet of salmon, skinned (tails are cheapest) </p> </li>
+	<li> <p>Any veggies you like: tomatoe, peppers, zuchchinis, etc </p> </li>
+	<li> <p>Liquid cream </p> </li>
+	<li> <p>Black pepper </p> </li>
+	<li> <p>Salt</p> </li>
+</ul>
+<ol>
+	<h3> Method </h3>  
+	<li> <p>1. Put the salmon in an oven dish - add chopped veggies and cream on top - with salt and pepper </p> </li>
+	<li> <p>2. Grill on a high heat for about 25 min. If it is a thick fillet, it will take longer to cook through, always check if the middle if cooked. </p> </li>
+	<li> <p>3. You can serve it with rice or pasta </p> </li>
+</ol>
+  </body>
+</html>
