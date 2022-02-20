549bbfb89bdfeaa33bf0b5843f912216518812f0
Lowelyn Ann B. De Paula <99851965+kcdepaula@users.noreply.github.com>
2022-02-20 20:53:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Welcome To My Website! My Name is Lowelyn Ann B. De Paula
+<!DOCTYPE html>
+<html>
+	<title>My Home Page</title>
+	<head><h1>Welcome To My Website! My Name is Lowelyn Ann B. De Paula. </h1></head>
+  <body>
+  <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="Recipes.php">Learning Task 2</a></li>
+    </ul>
+  </body>
+	
+</html>
