596f83fbb15a5d960f34f5eb9bc74aeec7b1ada7
Lowelyn Ann B. De Paula <99851965+kcdepaula@users.noreply.github.com>
2022-02-21 01:47:51 +0800

    Create PineappleChicken.php

---
+++ LT2/PineappleChicken.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Pineapple Chicken </title>
+  </head>
+  <body>
+	<h1> Pineapple Chicken </h1>
+	<img src="PineappleChicken.jpg" width= 450 height= 300 alt="Pineapple Chicken">
+    <h2> 4 Servings </h2>
+	<h3> Ingredients </h3>
+<ul>
+	<li> <p>2 tsp canila or sesame oil </p> </li>
+	<li> <p>1 (10-oz) package frozen broccoli (or stir - fry vegetable mix), thawed*</p> </li>
+	<li> <p>1/2 C stir-fry sauce </p> </li>
+	<li> <p>1/4 C pineapple juice, use reserved juive from canned pineapple </p> </li>
+	<li> <p>1/4 tsp garlic powder</p> </li>
+    <li> <p>1/4 tsp crushed red pepper (optional - use if you like it hot!) </p> </li>
+    <li> <p>1 (15-oz) can pineapple chunks or tidbits, drained (reserve the juice!) </p> </li>
+    <li> <p>2 C diced cooked chicken or 2 (10-oz) can chicken breast, drained and flaked </p> </li> 
+</ul>
+<ol>
+	<h2> Method </h2>
+	<li> <p>1) Heat the oil in a large skillet over medium high heat. Add all ingredients EXCEPT the pineapple and chicken. Cook and stir i=until heated  </p> </li>
+	<li> <p>2) Add pineapple and chicken; cook another 2 minutes. </p> </li>
+	<li> <p>3) Serve over instant brown rice or whole wheat pasta. </p> </li>
+	<li> <p>4) *Thaw frozen vegetables in the microwave or by holding the package under cold running water for several minutes. </p> </li>
+</ol>
+  </body>
+</html>
