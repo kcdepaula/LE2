3aba6cc2d7920a37e1c3cf5d0282535ad2159de1
Lowelyn Ann B. De Paula <99851965+kcdepaula@users.noreply.github.com>
2022-03-14 15:27:47 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> My Home Page </title>
+  </head>
+  <body>
+  <h1> Welcome to My Website! I am Lowelyn Ann! </h1>
+  <ul>
+    <li><a href="index.php" target="_blank"> HOME </a></li>
+    <li><a href="/LT2/Recipes.html" target= "_blank"> LEARNING TASK 2 </a> </li>
+    <li><a href="/LT3/index.html" target= "_blank"> LEARNING TASK 3 </a> </li>
+  </ul>
+  </body>
+</html>
